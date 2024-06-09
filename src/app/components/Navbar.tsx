38613db59565c0475f3359b0111b056ded5027b6
Rohit Singh Rawat <rohitzrawat2003@gmail.com>
2024-06-09 22:45:40 +0530

    "style change'"

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 type Props = {};
 export default function Navbar({}: Props) {
 	return (
-		<nav className='bg-slate-600 p-4 sticky top-0 drop-shadow-xl z-10'>
+		<nav className='bg-gray-800 p-4 sticky top-0 drop-shadow-xl z-10'>
 			<div className='prose prose-xl mx-auto flex justify-between flex-col sm:flex-row'>
 				<Link
 					href={'/'}
